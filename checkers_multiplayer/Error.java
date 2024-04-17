02c2a00435db88b876a966be55cee29e966ef29a
jmaganarivera <123403740+jmaganarivera@users.noreply.github.com>
2024-04-17 15:05:42 -0500

    Update Error.java
    Added Error.java

--- checkers_multiplayer/Error.java
+++ checkers_multiplayer/Error.java
@@ -1,5 +1,30 @@
 package checkers_multiplayer;
 
-public class Error {
+import java.io.Serializable;
+
+public class Error implements Serializable
+{
+	private String Err_message;
+	private int status_code;
+	
+	public Error(String err_message, int i ) 
+	{
+		this.Err_message = err_message;
+		this.status_code = i;
+	}
+
+	public String get_message()
+	{
+		return this.Err_message;
+		
+		
+	}
+	public int get_status_code()
+	{
+		return this.status_code;
+	}
+
+
 
 }
+
