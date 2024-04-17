1009c45bd9db73184c5b9d5a7ddc566bc859afe9
DJMack2000 <dmcallister@cub.uca.edu>
2024-04-17 14:20:34 -0500

    Finished Move Class
    The Move Class is finished

--- checkers_multiplayer/Move.java
+++ checkers_multiplayer/Move.java
@@ -1,5 +1,42 @@
 package checkers_multiplayer;
 
-public class Move {
+import java.io.Serializable;
 
+public class Move implements Serializable
+{
+	private int turn;
+	private int id;
+	
+	private int currentposX, currentposY, newposX, newposY;
+	
+	public Move(int turn, int currentposX, int currentposY, int newposX, int newposY)
+	{
+		this.turn = turn;
+		this.newposX = newposX;
+		this.newposY = newposY;
+		this.currentposX = currentposX;
+		this.currentposY = currentposY;
+	}
+	
+	public int getTurn()
+	{
+		return this.turn;
+	}
+	
+	public int getID()
+	{
+		return this.id;
+	}
+	
+	public int[] getNewPos()
+	{
+		int[] temp = {newposX, newposY};
+		return temp;
+	}
+	
+	public int[] getCurrentPos()
+	{
+		int[] temp = {currentposX, currentposY};
+		return temp;
+	}
 }
