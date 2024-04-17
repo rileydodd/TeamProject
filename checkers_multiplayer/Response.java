d78e01ade0e23d735cf435615dbe8f254f6b5b32
kasikoel <123388620+kasikoel@users.noreply.github.com>
2024-04-17 14:48:04 -0500

    Update Response.java

--- checkers_multiplayer/Response.java
+++ checkers_multiplayer/Response.java
@@ -1,5 +1,23 @@
 package checkers_multiplayer;
 
-public class Response {
+import java.io.Serializable;
 
+public class response implements Serializable
+{
+	public String[][] pieces;
+	public int[][] teams;
+	
+	public response(String[][] pieces, int[][] teams)
+	{
+		this.pieces = pieces;
+		this.teams = teams;
+	}
+	
+	
 }
+
+
+
+  
+
+
