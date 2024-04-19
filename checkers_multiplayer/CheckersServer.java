19282e3dbc6c194b6c24dc96126cbf9ff7c2fc08
DJMack2000 <dmcallister@cub.uca.edu>
2024-04-19 16:58:01 -0500

    CheckersServer Class
    Sorry, I had to make it more readable for me. But, I believe I have fixed the issue of the connection between client and server, at least on the server side...

--- checkers_multiplayer/CheckersServer.java
+++ checkers_multiplayer/CheckersServer.java
@@ -12,7 +12,8 @@ import java.lang.Runnable;
 
 import java.util.*;
 
-public class CheckersServer extends AbstractServer{
+public class CheckersServer extends AbstractServer
+{
 	private JTextArea log;
 	private JLabel status;
 	private DatabaseFile databasefile;
@@ -23,20 +24,31 @@ public class CheckersServer extends AbstractServer{
 	private int num_players;
 	private Piece[][] board;
 	
-	public CheckersServer() {
+	public CheckersServer()
+	{
 		super(8300);
 		num_players = 0;
 		
-		try {
+		try
+		{
 			databasefile = new DatabaseFile(true);
-		} catch (IOException e) {
+		} catch (IOException e)
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 	}
 	
-	public CheckersServer(int port, int timeout) {
+	
+	// Method that tells the user when the server has started.
+	public void serverStarted()
+	{
+		System.out.println("Server Started");
+	}
+	
+	public CheckersServer(int port, int timeout)
+	{
 		super(port);
 		num_players = 0;
 		this.setTimeout(timeout);
@@ -45,22 +57,28 @@ public class CheckersServer extends AbstractServer{
 		ArrayList<Piece> player_2_pieces = new ArrayList<Piece>();
 		
 		this.board = new Piece[8][8];
-		for (int i = 0; i < 8; i++) {
-		    for (int j = 0; j < 8; j++) {
+		for (int i = 0; i < 8; i++)
+		{
+			for (int j = 0; j < 8; j++)
+			{
 		        int[] pos = new int[2];
 		        pos[0] = i;
 		        pos[1] = j;
 
 		        // Set up white pieces
-		        if ((i + j) % 2 == 0 && i < 3) {
+		        if ((i + j) % 2 == 0 && i < 3)
+		        {
 		            board[i][j] = new CheckersPiece("Red", j, j, j, pos);
 		        }
+		        
 		        // Set up black pieces
-		        else if ((i + j) % 2 == 0 && i > 4) {
+		        else if ((i + j) % 2 == 0 && i > 4)
+		        {
 		            board[i][j] = new CheckersPiece("Black", j, j, j, pos);
 		        }
 		        // Set up empty squares
-		        else {
+		        else
+		        {
 		            board[i][j] = new Piece("Empty", j, j, j, pos);
 		        }
 		    }
@@ -71,9 +89,11 @@ public class CheckersServer extends AbstractServer{
 
 		    this.ce = new CheckersEngine(board, player1, player2);
 
-		    try {
+		    try
+		    {
 		    	this.listen();
-		    } catch (IOException e) {
+		    } catch (IOException e)
+		    {
 		    	// TODO Auto-generated catch block
 		    	e.printStackTrace();
 		    }
@@ -103,124 +123,138 @@ public class CheckersServer extends AbstractServer{
 
 	@Override
 	protected void handleMessageFromClient(Object arg0, ConnectionToClient arg1) {
-		if (arg0 instanceof Move) {
+		String msg = (String)arg0;
+		String msg1 = "Client " + arg1.getId() + msg;
+		  
+		System.out.println(msg1);
+		  
+		try
+		{
+			arg1.sendToClient(msg1);
+		}catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		  
+		if (arg0 instanceof Move)
+		{
 			String[][] pieces = new String[8][8];
 			int[][] teams = new int[8][8];
 
 			Move mv = (Move) arg0;
-			if (ce.validate(mv)) {
-				for (int i = 0; i < 8; i++) {
-					for (int j = 0; j < 8; j++) {
+			if (ce.validate(mv))
+			{
+				for (int i = 0; i < 8; i++)
+				{
+					for (int j = 0; j < 8; j++)
+					{
 						pieces[i][j] = ce.getBoard()[i][j].getName();
 						teams[i][j] = ce.getBoard()[i][j].getTeam();
 					}
 				}
-
 				this.sendToAllClients(new Response(pieces, teams));
-
 			}
-
 		}
 
 		 if (arg0 instanceof LoginData)
-		    {
-			   try {
-				arg1.sendToClient("START");
-			} catch (IOException e1) {
+		 {
+			 try 
+			 {
+				 arg1.sendToClient("START");
+			 } catch (IOException e1)
+			 {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
-			}
-		       LoginData loginData = (LoginData)arg0;
-		       System.out.print("dfasdfrfrf");
-				//databasefile.checkLogin(loginData.getUserName(), loginData.getPassword());
-				//boolean x = databasefile.checkLogin(loginData.getUserName(), loginData.getPassword());
-				String[] login_data = databasefile.load_data();
-				System.out.println("in login "+ x );
+			 }
+		     LoginData loginData = (LoginData)arg0;
+		     System.out.print("dfasdfrfrf");
+		     //databasefile.checkLogin(loginData.getUserName(), loginData.getPassword());
+		     //boolean x = databasefile.checkLogin(loginData.getUserName(), loginData.getPassword());
+		     String[] login_data = databasefile.load_data();
+		     System.out.println("in login "+ x );
 				
-				 for(int i = 0; i < login_data.length; i++)
-				  {
-					 System.out.println(login_data[i]);
-					  String[] temp = login_data[i].split("/");
-					  System.out.println(temp[0]);
-					  System.out.println(temp[1]);
+		     for(int i = 0; i < login_data.length; i++)
+		     {
+			
+		    	 System.out.println(login_data[i]);
+		    	 String[] temp = login_data[i].split("/");
+		    	 System.out.println(temp[0]);
+				
+		    	 System.out.println(temp[1]);
 
-					  if(!temp[1].contains(""+loginData.getUserName()) )
-					  {
+		    	 if(!temp[1].contains(""+loginData.getUserName()) )
+		    	 {
 						 
-						 if(!temp[1].contains(""+loginData.getPassword()) )
-						 {
-							 Error new_message = new Error("Password not exist", 2);
-							 try
-							{
-								arg1.sendToClient(new_message);
-							} catch (IOException e)
-							{
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-						 }
-						 else
-						 {
-							Error new_message = new Error("Welcome", 6);
-							try
-							{
-								arg1.sendToClient(new_message);
-							} catch (IOException e)
-							{
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-						 }
-					  }
-					  else
-						 {
-							Error new_message = new Error("Welcomme", 6);
-							try
-							{
-								arg1.sendToClient(new_message);
-							} catch (IOException e)
-							{
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-						 }
-					  
-				  }
-		       
-		    }
-		        else if (arg0 instanceof CreateAccountData)
-		        {
+		    		 if(!temp[1].contains(""+loginData.getPassword()) )
+		    		 {
+		    			 Error new_message = new Error("Password not exist", 2);
+		    			 try
+		    			 {
+		    				 arg1.sendToClient(new_message);
+		    			 } catch (IOException e)
+		    			 {
+		    				 // TODO Auto-generated catch block
+		    				 e.printStackTrace();
+		    			 }
+		    		 }
+		    		 else
+		    		 {
+		    			 Error new_message = new Error("Welcome", 6);
+		    			 try
+		    			 {
+		    				 arg1.sendToClient(new_message);
+		    			 } catch (IOException e)
+		    			 {
+		    				 // TODO Auto-generated catch block
+		    				 e.printStackTrace();
+		    			 }
+		    		 }
+		    	 }
+		    	 else
+		    	 {
+		    		 Error new_message = new Error("Welcomme", 6);
+		    		 try
+		    		 {
+		    			 arg1.sendToClient(new_message);
+		    		 } catch (IOException e)
+		    		 {
+		    			 // TODO Auto-generated catch block
+		    			 e.printStackTrace();
+		    		 }
+		    	 }	 
+		     }  
+		 }
+		 else if (arg0 instanceof CreateAccountData)
+		 {
 		           
-		           CreateAccountData creatAccountData = (CreateAccountData)arg0;
+			 CreateAccountData creatAccountData = (CreateAccountData)arg0;
 		         
-		           try {
-		        	   
-		        	 
-					databasefile.writeCredentials(creatAccountData.getUserName(), creatAccountData.getPassword());
-					x = databasefile.checkCredentials(creatAccountData.getUserName(), creatAccountData.getPassword() + "/n");
-					creatAccountData.setCheck(x);
-					if(x)
-					{
-						arg1.sendToClient(new Error("Username Exists", 1));
-								
-						return;
-					}
-					else
-					{
-						arg1.sendToClient(new Error(" ", 5));
-						
-					}
+			 try
+			 {   	 
+				 databasefile.writeCredentials(creatAccountData.getUserName(), creatAccountData.getPassword());
+				 x = databasefile.checkCredentials(creatAccountData.getUserName(), creatAccountData.getPassword() + "/n");
+				 creatAccountData.setCheck(x);
+				 if(x)
+				 {
+					 arg1.sendToClient(new Error("Username Exists", 1));		
+					 return;
+				 }
+				 else
+				 {
+					 arg1.sendToClient(new Error(" ", 5));	
+				 }
 					
-					System.out.println("in server "+ x );
-					//arg1.sendToClient(creatAccountData);
-		           } catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+				 System.out.println("in server "+ x );
+				 //arg1.sendToClient(creatAccountData);
+			 } catch (IOException e) 
+			 {
+				 // TODO Auto-generated catch block
+				 e.printStackTrace();
+			 }
 
-		           System.out.println("Username/Password    " + creatAccountData.getUserName() + "/" + creatAccountData.getPassword() + "/" + creatAccountData.getrePassword());
+			 System.out.println("Username/Password    " + creatAccountData.getUserName() + "/" + creatAccountData.getPassword() + "/" + creatAccountData.getrePassword());
 		           
-		        }
+		 }
 
 	}
 	
